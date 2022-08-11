571cc1d1cf0108ac8f50ed0252b18a430565b70e
Pedro Felipe Matos Menezes <pedro@nuvme.com.br>
2022-08-11 10:09:24 -0300

    local settings, db connection

--- api_sistema_vendas/produtos/admin.py
+++ api_sistema_vendas/produtos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Plataforma, FaixaEtaria, Genero, Categoria, Preco, Produto
 
-# Register your models here.
+admin.site.register(Plataforma)
+admin.site.register(FaixaEtaria)
+admin.site.register(Genero)
+admin.site.register(Categoria)
+admin.site.register(Preco)
+admin.site.register(Produto)
