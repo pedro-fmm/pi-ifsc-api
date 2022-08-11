47b9df3a5b374bf8e405af85e06e3a2156e45eda
Pedro Felipe Matos Menezes <pedro@nuvme.com.br>
2022-08-11 08:55:13 -0300

    models produto foreign key

--- api_sistema_vendas/produtos/models.py
+++ api_sistema_vendas/produtos/models.py
@@ -97,14 +97,12 @@ class Produto(models.Model):
     id                      = models.UUIDField(primary_key=True)
     nome                    = models.CharField(max_length=256)
     descricao               = models.TextField()
-    preco                   = models.DecimalField(max_digits=12, decimal_places=2)
     imagem                  = models.FileField(upload_to='media')
-    categoria               = models.ForeignKey(Categorias, on_delete=models.CASCADE, related_name='produtos')
-    tags                    = models.ManyToManyField(Tag, related_name='produtos')
+    plataforma              = models.ForeignKey('Plataforma', on_delete=models.CASCADE, related_name='produtos')
+    faixa_etaria            = models.ForeignKey('FaixaEtaria', on_delete=models.CASCADE, related_name='produtos')
+    genero                  = models.ForeignKey('Genero', on_delete=models.CASCADE, related_name='produtos')
+    categoria               = models.ForeignKey('Categoria', on_delete=models.CASCADE, related_name='produtos')
     data_criacao            = models.DateField(auto_now_add=True)
-    visualizacoes           = models.IntegerField()
-    vendas                  = models.IntegerField()
-    is_disponivel           = models.BooleanField(default=True)
     estoque                 = models.IntegerField(default=0) 
 
     def __str__(self):
