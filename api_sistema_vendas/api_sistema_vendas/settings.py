bce4a3c09be512e59082627bef5315eeb9249e1f
LucasFarias <lucas@nuvme.com.br>
2022-08-18 10:32:10 -0300

    hide secret key

--- api_sistema_vendas/api_sistema_vendas/settings.py
+++ api_sistema_vendas/api_sistema_vendas/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mws8w#9=8i)twc#hvw1*t1j2nc!p^#11#a=j8&ac64m%$3ll37'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
